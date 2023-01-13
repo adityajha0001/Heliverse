90c49bc2b4218b87e1221a8526025e3940d8e0fe
adityajha0001 <rockadi2407@gmail.com>
2023-01-13 22:37:19 +0530

    pagination

--- src/components/home/data.js
+++ src/components/home/data.js
@@ -1,11 +1,21 @@
 import INFOS from "../../data/heliverse_mock_data.json";
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { add } from "../../store/cartSlice";
-
+import React, { useState, useEffect } from 'react';  
+import { ReactPaginate } from 'react'
 
 
 export default function Data (){
+    const [users, setUsers] = useState(INFOS.slice(0,20));
+    const [pageNumber, setpageNumber ]= useState(0)
+
+    const usersPerPage =20
+    const pageVisited = pageNumber * usersPerPage
+
+
+
+    const displayUsers = users.slice(pageVisited, pageVisited + usersPerPage)
+
 
     const dispatch = useDispatch();
 
@@ -18,13 +28,19 @@ export default function Data (){
 
     }; 
 
+    const pageCount = Math.ceil(users.length/ usersPerPage);
+    const changePage = ({selected}) =>{
+        setpageNumber(selected);
+
+    }; 
+
+
     return(
         <>
         <div>
             <div className="pt-12 px-12">
                 <input className=" h-12 bg-gray-100  border-4  border-sky-500" type="text" placeholder="  Search..." onChange={(event)=>{setSearchTerm(event.target.value)}}/>
             </div>
-
         
             <div className=" grid grid-cols-4 px-8 py-8 ">
                  {INFOS.filter((INFO)=>{
@@ -37,7 +53,7 @@ export default function Data (){
                     }else if (INFO.gender.toLocaleUpperCase().includes(searchTerm.toLocaleUpperCase())){
                         return INFO
                     }
-                 }).map(INFO =>{
+                 }).slice(pageVisited,pageVisited + usersPerPage).map((INFO)=>{
                     return(
                         <>
                         <div className="">
@@ -52,21 +68,18 @@ export default function Data (){
                                     <h1 className="text-sm">{INFO.available}</h1>
                                     <button onClick={()=>handleAdd(INFO )} className="font-bold hover:bg-blue-400 text-sm bg-blue-600 rounded-lg px-4 py-2 text-white">Add to team</button>
                                 </div>
-                           
                             </div>
-                           
                         </div>
+
+                  
                         </>
     
-                    );
-
-
-                })}
-
+                    )})}
             </div>
         </div>
         </>
-    )
+    );
+    
 };
 
 
